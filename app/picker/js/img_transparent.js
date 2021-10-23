da29d953510b80e66ebea114c425df11cfe5eb69
YotioSoft <ytani0323@gmail.com>
2021-10-23 15:13:44 +0900

    picker: 作成中

--- app/picker/js/img_transparent.js
+++ app/picker/js/img_transparent.js
@@ -1,6 +1,7 @@
 function previewFile(file) {
     // 追加先
     const previewArea = document.getElementById('preview');
+    const appArea = document.getElementById('app-area-id');
 
     // キャンバスの準備
     var ctx = previewArea.getContext("2d");
@@ -22,16 +23,39 @@ function previewFile(file) {
     const image = new Image();
     var image_data = ctx.createImageData(256, 256);
     reader.onloadend = function() {
+        // 幅、高さの初期値
+        previewArea.width = appArea.width;
+        previewArea.height = appArea.height * 0.8;
+
+        console.log("previewArea_width = " + appArea.width);
+        console.log("previewArea_height = " + appArea.height);
+        
         image.src = reader.result;
+
         image.onload = () => {
-            previewArea.width = image.width;
-            previewArea.height = image.height;
-            ctx.drawImage(image, 0, 0);
-            
+            var canvas_hpw = previewArea.height / previewArea.width;
+            var image_hpw = image.height / image.width;
+
+            var resized_image_width;
+            var resized_image_height;
+
+            if (image_hpw <= canvas_hpw) {
+                resized_image_width  = previewArea.width / image.width * image.height;
+                resized_image_height = previewArea.height;
+            }
+            else {
+                resized_image_height = previewArea.height / image.height * image.width;
+                resized_image_width  = previewArea.width;
+            }
+            console.log("resized_image_width = " + resized_image_width);
+            console.log("resized_image_height = " + resized_image_height);
+
+            ctx.drawImage(image, 0, 0, resized_image_width, resized_image_height);
+
             // 読み込んだ画像からImageDataを取得
             console.log(ctx.width);
             console.log(parseInt(ctx.width));
-            image_data = ctx.getImageData(0, 0, image.width, image.height);
+            image_data = ctx.getImageData(0, 0, resized_image_width, resized_image_height);
         }
     }
 
